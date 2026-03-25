70d8e194f18f939744080c97f77563ac067f6bb3
3030Andre <Andre.v2005@aluno.ifsc.edu.br>
2026-03-25 08:27:36 -0300

    atualizando banco

--- src/database/pizzaria_db.sql
+++ src/database/pizzaria_db.sql
@@ -31,10 +31,10 @@ CREATE TABLE pizzas (
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO pizzas (nome, descricao, preco) VALUES
+INSERT INTO pizzas (nome, descricao, preco, img) VALUES
 ('Banana com canela', 'Molho de tomate, mussarela, banana e canela', 90.00, '/img/pizzas/bananacomcanela.jpg'),
 ('Frango com catupiry', 'Molho de tomate, mussarela, frango e catupiry', 90.00, '/img/pizzas/Frangocatupiry.jpg'),
-('Pizzadecalabresa', 'Molho de tomate, mussarela, calabresa e orégano s/Pizzadecalabresa.jpg'),
-('Pizzadecarnemoida', 'Molho de tomate, mussarela, carnemoida, 90.00', '/img/pizzas/Pizzadecarnemoida'),
+('Pizzadecalabresa', 'Molho de tomate, mussarela, calabresa e orégano', 90.00, '/img/pizzas/Pizzadecalabresa.jpg'),
+('Pizzadecarnemoida', 'Molho de tomate, mussarela, carnemoida', 90.00, '/img/pizzas/Pizzadecarnemoida'),
 ('Pizzadebacon', 'Molho de tomate, mussarela, bacon e orégano', 90.00, '/img/pizzas/Pizzadebacon.jpg'),
 ('Pizzaportuguesa', 'Molho de tomate, mussarela, presunto, ovo, cebola e orégano', 90.00, '/img/pizzas/Pizzaportuguesa.jpg')
