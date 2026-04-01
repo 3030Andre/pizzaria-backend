b0a368c263e5f386fdb0d59dac36d1cb226131c7
Andre3030 <Andre.v2005@aluno.ifsc.edu.br>
2026-04-01 10:07:35 -0300

    tela de cadastro

--- index.js
+++ index.js
@@ -5,15 +5,20 @@ const db = require('./src/database/db');
 
 // configura a pasta publica
 app.use(express.static('public'));
+
 // seta o hbs
 app.set('view engine', 'hbs')
 
+// Middleware para ler dados do POST
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/', async (req, res) => {
+  await db.runSQLFile(); // cria o banco
   res.render('principal')
 })
 
 app.get('/pedido', async (req, res) => {
-  await db.runSQLFile(); // cria o banco
   const pizzas = await db.query('SELECT * FROM pizzas');
   res.render('pedidos', {pizzas})
 })
@@ -23,7 +28,17 @@ app.get('/item-selecionado', async (req, res) => {
 })
 
 app.get('/cadastro-pizza', async (req, res) => {
-  res.render('cadastroPizza')
+  res.render('cadastroPizza');
+})
+
+app.post('/cadastro-pizza', async (req, res) => {
+  console.log(req.body);
+  if(req.body)
+  {
+    const pizzas = await db.query("INSERT INTO pizzas (nome, descricao, preco, img) VALUES (?, ?, ?, ?)", [req.body.nome, req.body.descricao, req.body.preco, req.body.imagem]);
+    console.log(pizzas)
+  }
+  res.redirect("/pedido");
 })
 
 app.listen(port, async () => {
