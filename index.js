e796400e2109f6abf595bba9afa080fe36b8387b
Your Name <Andre.v2005@aluno.ifsc.edu.br>
2026-05-06 08:09:15 -0300

    sistema de login

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ const port = 3000
 const db = require('./src/database/db');
 const multer = require("multer");
 const path = require("path");
+const session = require('express-session');
 
 const app = express();
+
+// configuração de session
+app.use(session({
+  secret: 'seu_segredo_aqui',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false } // true só com HTTPS
+}));
+
+function verificarLogin(req, res, next) {
+  if (req.session.usuario) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+}
 
 // Configuração de armazenamento
 const storage = multer.diskStorage({
@@ -34,20 +51,20 @@ app.get('/', async (req, res) => {
   res.render('principal')
 })
 
-app.get('/pedido', async (req, res) => {
+app.get('/pedido', verificarLogin, async (req, res) => {
   const pizzas = await db.query('SELECT * FROM pizzas');
   res.render('pedidos', {pizzas})
 })
 
-app.get('/item-selecionado', async (req, res) => {
+app.get('/item-selecionado', verificarLogin, async (req, res) => {
   res.render('item-selecionado')
 })
 
-app.get('/cadastro-pizza', async (req, res) => {
+app.get('/cadastro-pizza', verificarLogin, async (req, res) => {
   res.render('cadastroPizza');
 })
 
-app.post('/cadastro-pizza', upload.single("imagem"), async (req, res) => {
+app.post('/cadastro-pizza', verificarLogin, upload.single("imagem"), async (req, res) => {
   console.log(req.body);
   console.log(req.file);
   const caminhoImagem = "/uploads/" + req.file.filename;
@@ -82,7 +99,30 @@ app.post('/cadastro-usuario', async (req, res) => {
   res.redirect("/");
 })
 
-app.get('/carrinho', async (req, res) => {
+app.post('/login-usuario', async (req, res) => {
+  if (req.body) {
+    const user = await db.query(
+      "SELECT * FROM usuarios WHERE email = ?",
+      [req.body.email]
+    );
+
+    if (user[0] && user[0].senha == req.body.senha) {
+      console.log("senha correta");
+
+      // 🔑 salva na sessão
+      req.session.usuario = {
+        id: user[0].id,
+        email: user[0].email
+      };
+
+      return res.redirect("/");
+    }
+  }
+
+  res.redirect("/login");
+});
+
+app.get('/carrinho', verificarLogin, async (req, res) => {
   res.render('carrinho');
 })
 
